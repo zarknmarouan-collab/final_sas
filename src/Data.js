4529c7017dbc85d401acf3b95a723145a6011090
fouadlamrini <fouad11.lamrini@gmail.com>
2026-09-17 09:45:00 +0000

    initialiser data de fichier data.js

---
+++ src/Data.js
@@ -0,0 +1,90 @@
+
+const apprenants = [
+
+  {
+    id: 1,
+    nomComplet: "Sara Dev",
+    ville: "Nador",
+    resultats: [
+      { jour: 1, exercicesTermines: 18, totalExercices: 20, challengeTermine: true },
+      { jour: 2, exercicesTermines: 14, totalExercices: 20, challengeTermine: false }
+    ]
+  },
+
+  {
+    id: 2,
+    nomComplet: "Yassine Code",
+    ville: "Oujda",
+    resultats: [
+      { jour: 1, exercicesTermines: 12, totalExercices: 20, challengeTermine: false }
+    ]
+  },
+
+  {
+    id: 3,
+    nomComplet: "Mohamed Java",
+    ville: "Rabat",
+    resultats: [
+      { jour: 1, exercicesTermines: 17, totalExercices: 20, challengeTermine: true },
+      { jour: 2, exercicesTermines: 19, totalExercices: 20, challengeTermine: true }
+    ]
+  },
+
+  {
+    id: 4,
+    nomComplet: "Salma Web",
+    ville: "Casablanca",
+    resultats: [
+      { jour: 1, exercicesTermines: 11, totalExercices: 20, challengeTermine: false },
+      { jour: 2, exercicesTermines: 13, totalExercices: 20, challengeTermine: true },
+      { jour: 3, exercicesTermines: 15, totalExercices: 20, challengeTermine: false }
+    ]
+  },
+
+  {
+    id: 5,
+    nomComplet: "Ayoub Dev",
+    ville: "Tanger",
+    resultats: [
+      { jour: 1, exercicesTermines: 20, totalExercices: 20, challengeTermine: true },
+      { jour: 2, exercicesTermines: 18, totalExercices: 20, challengeTermine: true },
+      { jour: 3, exercicesTermines: 16, totalExercices: 20, challengeTermine: true }
+    ]
+  },
+
+  {
+    id: 6,
+    nomComplet: "Hajar Code",
+    ville: "Fès",
+    resultats: [
+      { jour: 1, exercicesTermines: 9, totalExercices: 20, challengeTermine: false },
+      { jour: 2, exercicesTermines: 10, totalExercices: 20, challengeTermine: false },
+      { jour: 3, exercicesTermines: 12, totalExercices: 20, challengeTermine: true }
+    ]
+  },
+
+  {
+    id: 7,
+    nomComplet: "Ilyass FullStack",
+    ville: "Marrakech",
+    resultats: [
+      { jour: 1, exercicesTermines: 16, totalExercices: 20, challengeTermine: true },
+      { jour: 2, exercicesTermines: 15, totalExercices: 20, challengeTermine: false },
+      { jour: 3, exercicesTermines: 18, totalExercices: 20, challengeTermine: true },
+      { jour: 4, exercicesTermines: 17, totalExercices: 20, challengeTermine: true }
+    ]
+  },
+
+  {
+    id: 8,
+    nomComplet: "Nadia JavaScript",
+    ville: "Agadir",
+    resultats: [
+      { jour: 1, exercicesTermines: 7, totalExercices: 20, challengeTermine: false },
+      { jour: 2, exercicesTermines: 11, totalExercices: 20, challengeTermine: true }
+    ]
+  }
+
+];
+
+module.exports = apprenants;

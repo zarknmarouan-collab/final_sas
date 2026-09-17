114b1b288cec1521661f268dfaf28b36300eaae1
fouadlamrini <fouad11.lamrini@gmail.com>
2026-09-17 14:30:00 +0000

    creer fonction enregistrer Resultat

--- src/fonction.js
+++ src/fonction.js
@@ -25,3 +25,41 @@ function ajouterApprenant(nom, ville) {
 }
 
 // //************************************* */
+function enregistrerResultat(
+  id,
+  jour,
+  exercicesPropose,
+  exerciceTermine,
+  challengeTermine,
+) {
+  let apprenant = apprenants.find(function (apprenant) {
+    return apprenant.id === id;
+  });
+
+  if (apprenant === undefined) {
+    return false;
+  }
+
+  let resultat = apprenant.resultats.find(function (resultat) {
+    return resultat.jour === jour;
+  });
+
+  if (resultat !== undefined) {
+    resultat.exercicesTermines = exerciceTermine;
+    resultat.totalExercices = exercicesPropose;
+    resultat.challengeTermine = challengeTermine;
+
+    return true;
+  } else {
+    apprenant.resultats.push({
+      jour: jour,
+      exercicesTermines: exerciceTermine,
+      totalExercices: exercicesPropose,
+      challengeTermine: challengeTermine,
+    });
+
+    return true;
+  }
+}
+
+// //************************************* */

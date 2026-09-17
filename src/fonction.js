a11738519872d091a9c47a0280e16707d38e0825
fouadlamrini <fouad11.lamrini@gmail.com>
2026-09-17 11:45:00 +0000

    creer fonction valider resultat

--- src/fonction.js
+++ src/fonction.js
@@ -4,3 +4,12 @@ function normaliserNom(nom) {
   let a = nom.trim();
   return a;
 }
+function validerResultat(realises, proposes) {
+  if (realises < 0 || proposes < 0) {
+    return false;
+  } else if (realises > proposes) {
+    return false;
+  }
+  return true;
+}
+//==============================================/

0dcc0b6271c412b62e8e2a0b04c9229483f6a2e6
fouadlamrini <fouad11.lamrini@gmail.com>
2026-09-18 19:30:00 +0000

    afficher les messages initial de dashboard

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const prompt = require("prompt-sync")();
+const apprenants = require("./Data");
+
+const {
+  ajouterApprenant,
+  enregistrerResultat,
+  rechercherApprenant,
+  calculerProgression,
+  afficherApprenants,
+  afficherTableauDeBord,
+  trierAlphabetique,
+  filtrerParNiveau,
+  trierParProgression,
+} = require("./fonctions");
+let choix;
+
+do {
+console.log("\n====== MENU PRINCIPAL ======");
+
+  console.log("1. Afficher le tableau de bord");
+  console.log("2. Afficher la liste des apprenants");
+  console.log("3. Ajouter un apprenant");
+  console.log("4. Consulter un apprenant par identifiant");
+  console.log("5. Rechercher un apprenant par nom");
+  console.log("6. Ajouter ou modifier un résultat");
+  console.log("7. Filtrer les apprenants par niveau");
+  console.log("8. Trier les apprenants");
+  console.log("0. Quitter");
+
+  choix = prompt("Votre choix : ").trim();
+
+} while (choix !== "0");
